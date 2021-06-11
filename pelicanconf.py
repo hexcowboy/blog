eb1b7dc955e9ab6d0777bd4bcd57b6bb8806b606
hexcowboy <hex@cowboy.dev>
2021-06-11 18:12:43 -0500

    initial commit

---
+++ pelicanconf.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*- #
+
+AUTHOR = 'hexcowboy'
+SITENAME = 'hexcowboy'
+# SITEURL = 'https://cowboy.dev'
+
+THEME = 'themes/cowboy'
+PATH = 'content'
+
+TIMEZONE = 'America/Chicago'
+
+DEFAULT_LANG = 'en'
+
+# Feed generation is usually not desired when developing
+FEED_ALL_ATOM = None
+CATEGORY_FEED_ATOM = None
+TRANSLATION_FEED_ATOM = None
+AUTHOR_FEED_ATOM = None
+AUTHOR_FEED_RSS = None
+
+# Blogroll
+# LINKS = (('About', ''),)
+
+# Social widget
+SOCIAL = (('Discord', 'https://discordapp.com/users/418557177825853443'),
+         ('GitHub', 'https://github.com/hexcowboy'),
+         ('Twitter', 'https://twitter.com/hexcowboy'),)
+
+DEFAULT_PAGINATION = 10
+
+# Uncomment following line if you want document-relative URLs when developing
+#RELATIVE_URLS = True
